38642783d4c1a686d0109f9c9bd6cb432e9132a3
amusictheorist <a_millaresthomson@outlook.com>
2024-12-11 16:13:59 -0500

    Add nodemon and morgan dependencies

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
+// Setup
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const PORT = 8080;
+
+// Middlewware
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
