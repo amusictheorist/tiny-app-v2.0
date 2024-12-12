9b836dfe6f30fe4bebc4f950df363cdbee19cc21
amusictheorist <a_millaresthomson@outlook.com>
2024-12-12 17:46:35 -0500

    resetting to earlier version

--- helpers.js
+++ helpers.js
@@ -5,4 +5,29 @@ function generateRandomString() {
   return result;
 };
 
-module.exports = { generateRandomString };
+// Mock Database
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// Mock users
+const users = {
+  'xarknlmie3i': {
+    id: 'xarknlmie3i',
+    email: 'a@a.com',
+    password: 'asdf'
+  },
+  '4ruu71bhvyo': {
+    id: '4ruu71bhvyo',
+    email: 'b@b.com',
+    password: 'qwer'
+  },
+  'x3yuka76ylk': {
+    id: 'x3yuka76ylk',
+    email: 'c@c.com',
+    password: 'zxcv'
+  }
+};
+
+module.exports = { generateRandomString, urlDatabase, users };
--- routes/urls.js
+++ routes/urls.js
@@ -1,66 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { generateRandomString } = require('../helpers');
-
-// Mock Database
-const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 // Route for home
 router.get('/', (req, res) => {
-  res.redirect('/urls');
+  res.send('Hello!');
 });
 
 // GET /urls
 router.get('/urls', (req, res) => {
-  const templateVars = {
-    username: req.cookies['username'],
-    urls: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
-
-// GET /urls/new
-router.get('/urls/new', (req, res) => {
-  const username = req.cookies.username;
-  res.render('urls_new', { username });
-});
-
-// GET /urls/:id
-router.get('/urls/:id', (req, res) => {
-  const templateVars = {
-    username: req.cookies['username'],
-    id: req.params.id,
-    longURL: urlDatabase[req.params.id]
-  };
-  res.render('urls_show', templateVars);
-});
-
-// POST /urls
-router.post('/urls', (req, res) => {
-  const longURL = req.body.longURL;
-  const id = generateRandomString();
-  urlDatabase[id] = longURL;
-  res.redirect(`urls/${id}`);
-});
-
-// GET /u/:id
-router.get('/u/:id', (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[id];
-  if (!longURL) {
-    return res.status(404).send('URL not found');
-  }
-  res.redirect(longURL);  
-});
-
-// POST /urls/:id/delete
-router.post('/urls/:id/delete', (req, res) => {
-  const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect('/urls');
-});
-
 
 module.exports = router;
